85f4793871635a254e1a568503b9a738bc1fbd79
William Dickson <william.dickson@uconn.edu>
2014-09-18 11:08:50 -0400

    ex1 complete

--- hw3/ex1.c
+++ hw3/ex1.c
@@ -5,6 +5,10 @@ int is_vowel(char c)
     /*
      returns 1 if c is a vowels, 0 otherwise
     */
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+        return 1;
+    return 0;
 }
 
 int main()
@@ -13,4 +17,16 @@ int main()
      read text from standard input and write to the standard 
      output the text with the vowels removed
     */
+    char string[1001];
+
+    printf("Enter a string:");
+    fgets(string, 1001, stdin);
+
+    int i;
+    for(i = 0; string[i] != 0; i++){
+        if(!is_vowel(string[i])){
+            printf("%c", string[i]);
+        }
+    }
+    return 0;
 }
