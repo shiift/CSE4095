1762b46b4f7c193469bd63c59dd3813180b61c0f
Laurent Michel <ldm@engr.uconn.edu>
2014-09-25 11:23:08 -0400

    hw4

---
+++ hw4/wlist.c
@@ -0,0 +1,13 @@
+#include "wlist.h"
+
+Hashtable* readWordList(char* filename)
+{
+	/* Read a list of pairs from filename and construct a 
+		brand new Hashtable that contains all the pairs in the file. 
+		Namely, if the file contains a pair (a,b), then the hash table
+		should contain both a->b and b->a. 
+
+		Make sure that the hashtable is large enough to accomodate 
+		your list of pairs. Use a prime number for the size. 		
+		*/
+}
