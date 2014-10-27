0ab6c8c413b43c66365cca3d630acfc1c30103a9
William Dickson <william.dickson@uconn.edu>
2014-10-27 19:47:49 -0400

    finished

--- lab7/ex1.c
+++ lab7/ex1.c
@@ -8,29 +8,30 @@ int x;
 
 void increase(int cnt)
 {
-   int i;
-   for(i=0;i<cnt;i++) {
-      pthread_mutex_lock(&m);
-      x = x + 1;
-   }
+    int i;
+    for(i=0;i<cnt;i++) {
+        pthread_mutex_lock(&m);
+        x = x + 1;
+        pthread_mutex_unlock(&m);
+    }
 }
 
 int main(int argc,char* argv[])
 {
-   pthread_t tid1,tid2;
-   int status;
-   long cnt = atol(argv[1]);
-   pthread_mutex_init(&m,NULL);
+    pthread_t tid1,tid2;
+    long cnt = atol(argv[1]);
+    x = 0;
+    pthread_mutex_init(&m,NULL);
 
-   status = pthread_create(&tid1,NULL,(void*(*)(void*))increase,(void*)cnt);
-   status = pthread_create(&tid2,NULL,(void*(*)(void*))increase,(void*)cnt);
+    pthread_create(&tid1,NULL,(void*(*)(void*))increase,(void*)cnt);
+    pthread_create(&tid2,NULL,(void*(*)(void*))increase,(void*)cnt);
 
-   pthread_mutex_destroy(&m);
-   void *v1,*v2;
-   pthread_join(tid1,&v1);
-   pthread_join(tid1,&v2);
-   printf("counter is %d\n",x);
-   return 0;
+    void *v1,*v2;
+    pthread_join(tid1,&v1);
+    pthread_join(tid2,&v2);
+    pthread_mutex_destroy(&m);
+    printf("counter is %d\n",x);
+    return 0;
 }
 
 
