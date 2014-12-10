55b212608523ab1c12935b8462e95d24cfa7ae92
William Dickson <william.dickson@uconn.edu>
2014-12-10 16:03:24 -0500

    commit 1

---
+++ final/ex3_fixed.c
@@ -0,0 +1,43 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ThreadArg_tag {
+   int i;
+   char fileName[128];
+} ThreadArg;
+
+void* doWork(ThreadArg* arg)
+{
+   return NULL;
+}
+
+void readString(char* buf,size_t sz)
+{
+   char* end = buf + sz;
+   char ch;
+   while((ch=getchar()) != EOF && ch != '\n' && buf < end)
+      *buf++ = ch;
+   *buf = 0;   
+}
+
+int main(int argc,char* argv[])
+{
+   int i;
+   int nbThreads = atoi(argv[1]);
+   char buf[128];
+   pthread_t minion[nbThreads];
+   for(i=0;i < nbThreads;i++) {
+      printf("give a filename: ");
+      readString(buf,sizeof(buf)-1);
+      printf("Got [%s]\n",buf);
+      ThreadArg arg;
+      arg.i = i;
+      strncpy(arg.fileName,buf,sizeof(buf)-1);
+      pthread_create(minion+i,NULL,(void*(*)(void*))doWork,&arg);
+   }
+   for(i=0;i < nbThreads;i++)
+      pthread_join(minion[i],NULL);
+   return 0;
+}
---
+++ final/ex4_fixed.c
@@ -0,0 +1,50 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static long long t[1024] = {0};
+
+void silly(long n,pthread_rwlock_t* rwl)
+{
+   pthread_rwlock_unlock(rwl);
+   if (n==0) {
+      pthread_rwlock_wrlock(rwl);
+      t[0] = 1;
+      pthread_rwlock_unlock(rwl);
+   } else {
+      pthread_rwlock_rdlock(rwl);
+      if (t[n-1]==0)
+         silly(n-1,rwl);
+      pthread_rwlock_wrlock(rwl);
+      t[n] =  n + t[n-1];      
+      pthread_rwlock_unlock(rwl);
+   }
+}
+
+void *doWork(pthread_rwlock_t* rwl)
+{
+   long sv = ((long) ((long)pthread_self() >> 16))  % 32;
+   pthread_rwlock_rdlock(rwl);
+   silly(sv,rwl);
+   pthread_rwlock_rdlock(rwl);
+   printf("called by %p with %ld got: %lld\n",pthread_self(),sv,t[sv]);
+   pthread_rwlock_unlock(rwl);
+   return NULL;
+}
+
+int main(int argc,char* argv[])
+{
+   int i;
+   int nb = atoi(argv[1]);
+   pthread_rwlock_t rwl;
+   pthread_rwlock_init(&rwl,NULL);
+   pthread_t minion[nb];
+   for(i=0;i < nb;i++) {
+      pthread_create(minion+i,NULL,(void*(*)(void*))doWork,&rwl);
+   }
+   for(i=0;i < nb;i++)
+      pthread_join(minion[i],NULL);   
+   pthread_rwlock_destroy(&rwl);
+   return 0;
+}
